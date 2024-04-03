ce371f8300b2012e0b4e18e9880cb2beb47641a1
gamcdonald123 <gamcdonald@icloud.com>
2024-04-03 13:04:44 +0100

    passwords can be created

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,4 +1,11 @@
 class Password < ApplicationRecord
   has_many :user_passwords
   has_many :users, through: :user_passwords
+
+  encrypts :password
+  encrypts :username, deterministic: true
+
+  validates :username, presence: true
+  validates :url, presence: true
+  validates :password, presence: true
 end
